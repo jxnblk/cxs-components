8e6b2464ae50731346a4699e8220678acb2d563e
Brent Jackson <jxnblk@gmail.com>
2016-11-03 23:21:53 -0400

    Rebuild

--- docs/Tweet.js
+++ docs/Tweet.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Tweet = () => (
   <a href='https://twitter.com/share'
     className='twitter-share-button'
-    data-text='hello'
+    data-text='cxs-components: styled UI component primitives for React'
     data-via='jxnblk'>
     Tweet
   </a>
